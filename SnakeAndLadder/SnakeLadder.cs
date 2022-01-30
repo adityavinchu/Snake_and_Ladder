f53356c260c0f6d781ac2c70f6191fab2154d0af
aditya <vinchuaditya@gmail.com>
2022-01-30 14:57:47 +0530

    Initial

---
+++ SnakeAndLadder/SnakeLadder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SnakeAndLadder
+{
+    public class SnakeLadder
+    {
+        int player_position = 0;
+    }
+}
